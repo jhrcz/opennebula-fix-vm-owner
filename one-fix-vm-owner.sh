6d560805e71735af774fd48cc918c8725f7b19c0
Jan Horacek (jahor/jhrcz) <jahor@jhr.cz>
2013-03-06 10:44:08 +0100

    initial

---
+++ one-fix-vm-owner.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+sleep 1
+
+######################################################################
+#
+# Changes owner of opennebula VMs to owner of vm template
+#
+# Script operates on all "live" VMs or on one define by argument.
+# The second case is useful for use as a VM_HOOK to automagicaly
+# set VM names.
+#
+######################################################################
+
+# function for simple debugging condition
+# use: qdebug && echo debug message
+function qdebug
+{
+	if [ "$DEBUG" = "YES" ]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+# be verbose and write detailed trace to logfile
+qdebug && \
+	set -x
+qdebug && \
+	exec 2>/tmp/debug-$$.log
+
+######################################################################
+# CONFIGURATION
+######################################################################
+
+# command to "connect" to the one db
+DB="sqlite3 /var/lib/one/one.db"
+
+######################################################################
+# END CONFIGURATION
+######################################################################
+
+# function for running db queries
+# use: dbq "dome sql query;"
+function dbq
+{
+  echo "$1" | $DB
+}
+
+( if [ -n "$1" ]
+then
+	echo "$1"
+else
+	# 6=done, 7=failed
+	#dbq "select oid from vm_pool where state not in  ( 6, 7 ) and name like 'one-%';"
+	dbq "select oid from vm_pool where name like '${oldname_prefix}%';"
+fi )| while read oid
+do
+	qdebug && \
+		dbq "select name from vm_pool where oid = $oid;" | sed -e "s/^/DEBUG:/"
+	qdebug && \
+		dbq "select body from vm_pool where oid = $oid;" | sed -e "s/^/DEBUG:/"
+
+	templateid=$(dbq "select body from vm_pool where oid = $oid;"  | xmlstarlet fo | xmlstarlet sel -t -v '//VM/TEMPLATE/TEMPLATE_ID')
+	[ -n "$templateid" ] || continue
+	newowner=$(dbq "select uid from template_pool where oid = $templateid;")
+
+	# treat any error before new name is detected
+	if [ -n "$newowner" ]
+	then
+		qdebug && \
+			echo "DEBUG: $newowner"
+		
+		onevm chown $oid $newowner
+	fi
+
+done
+
